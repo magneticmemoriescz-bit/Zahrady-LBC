f2182e5d3d3a753b9d27a57bfcd395b405900e60
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2026-03-29 15:06:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      include: ['@emailjs/browser'],
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
